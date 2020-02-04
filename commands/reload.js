ef4707ac6c72cd8c1e90a6e834fed0a6d227cd92
lumap <55940580+lumap@users.noreply.github.com>
2020-02-04 17:44:06 +0100

    Create reload.js

---
+++ commands/reload.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: 'reload',
+    description: 'Reloads a command',
+    category: 'owner',
+    execute(client,message,args) {
+        if (!args) return;
+      const commandName = args;
+  if(!client.commands.has(commandName)) {
+    return message.reply("That command does not exist");
+  }
+  delete require.cache[require.resolve(`./${commandName}.js`)];
+  client.commands.delete(commandName);
+  const props = require(`./${commandName}.js`);
+  client.commands.set(commandName, props);
+  message.reply(`The command ${commandName} has been reloaded`);
+},
+};
+ 
