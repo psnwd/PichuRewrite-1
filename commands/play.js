9ddec4f2ddd8502b9a4e8c605efd9baaa9f19c9d
lumap <55940580+lumap@users.noreply.github.com>
2020-02-05 16:29:21 +0100

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -9,17 +9,17 @@ module.exports = {
         // If there's already a song playing 
         if(aSongIsAlreadyPlaying){
             // Add the song to the queue
-            let song = await client.player.addToQueue(message.guild.id, args[0]);
+            let song = await client.player.addToQueue(message.guild.id, args.join(' '));
             return message.channel.send(`${song.name} added to queue!`);
         } 
             // Else, play the song
-            client.player.play(message.member.voice.channel, args[0]).then((song) => {
+            client.player.play(message.member.voice.channel, args.join(' ')).then((song) => {
             message.channel.send(`Currently playing ${song.name}!`);
         }).catch(() => {
-            message.channel.send(`No song found for ${args[0]}`);
+            message.channel.send(`No song found for ${args.join(' ')}`);
         });
         
-        let song = await client.player.play(message.member.voice.channel, args[0]);
+        let song = await client.player.play(message.member.voice.channel, args.join(' '));
             song.queue.on('end', () => {
             message.channel.send('The queue is empty, leaving voice channel...');
         });
