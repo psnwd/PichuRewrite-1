ae5efb092a78eb99c2d934965ff282f31766b09d
lumap <55940580+lumap@users.noreply.github.com>
2020-02-05 16:06:23 +0100

    Update nowplaying.js

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -4,6 +4,8 @@ module.exports = {
     description: 'Shows the current song',
     category: 'music',
     async execute(client,message) {
+        let playing = client.player.isPlaying(message.guild.id);
+        if(!playing) return message.channel.send(':x: No songs currently playing!');
          let song = await client.player.nowPlaying(message.guild.id);
         message.channel.send(`Currently playing ${song.name}...`);
     },
