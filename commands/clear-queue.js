96dfecd731c38a350e23bffabe6f355698fd614f
lumap <55940580+lumap@users.noreply.github.com>
2020-02-05 15:58:31 +0100

    Update clear-queue.js

--- commands/clear-queue.js
+++ commands/clear-queue.js
@@ -3,8 +3,6 @@ module.exports = {
     description: 'Clears the queue',
     category: 'music',
     execute(client,message) {
-    let queue = await client.player.getQueue(message.guild.id);
-    if (!queue) return message.channel.send('There is no queue in this server')
          client.player.clearQueue(message.guild.id);
         message.channel.send('Queue cleared!');
     }
