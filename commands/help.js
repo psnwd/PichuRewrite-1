e686c626e3081a56691c8c8a451a98dbcf178c7e
lumap <55940580+lumap@users.noreply.github.com>
2020-02-05 13:39:46 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,15 +1,22 @@
+const {MessageEmbed} = require('discord.js')
 module.exports = {
     name: 'help',
     description: 'Shows the help',
     category: 'misc',
     execute(client,message,args) {
-const Discord = require('discord.js')
-list=[]
-client.commands.forEach(command => {
-    list.push('Name : '+command.name+', Description : '+command.description+', Category : '+command.category)
-});
-
-list.join(`\n`)
-message.channel.send(list)
+        if (!args) {
+            const embed = new MessageEmbed()
+            .setColor('RANDOM')
+            .setTitle('Pichu\'s help!')
+            }
+//    
+//list=[]
+//client.commands.forEach(command => {
+//    list.push('Name : '+command.name+', Description : '+command.description+', Category : '+command.category)
+//});
+//
+//list.join(`\n`)
+//message.channel.send(list)
+//
     },
 };
