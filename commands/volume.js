1f0a2a09026b8df0a26a016a2e4f5b674c900141
lumap <55940580+lumap@users.noreply.github.com>
2020-02-05 16:36:34 +0100

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -3,7 +3,7 @@ module.exports = {
     name: 'volume',
     description: 'Changes the volume',
     category: 'music',
-    async execute(client,message) {
+    async execute(client,message,args) {
        client.player.setVolume(message.guild.id, parseInt(args[0]));
         message.channel.send(`Volume set to ${args[0]} !`);
     },
