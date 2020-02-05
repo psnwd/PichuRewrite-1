c04b799c5e2eecede584bf572fbb4b9ed00f5244
lumap <55940580+lumap@users.noreply.github.com>
2020-02-05 16:26:23 +0100

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -4,20 +4,21 @@ module.exports = {
     category: 'music',
     async execute(client,message,args) {
         if (!args) return message.channel.Send('Please provide a song name or a youtube url!')
+        
         let aSongIsAlreadyPlaying = client.player.isPlaying(message.guild.id);
         // If there's already a song playing 
         if(aSongIsAlreadyPlaying){
             // Add the song to the queue
             let song = await client.player.addToQueue(message.guild.id, args[0]);
-            message.channel.send(`${song.name} added to queue!`);
-        } else {
+            return message.channel.send(`${song.name} added to queue!`);
+        } 
             // Else, play the song
             client.player.play(message.member.voice.channel, args[0]).then((song) => {
             message.channel.send(`Currently playing ${song.name}!`);
         }).catch(() => {
             message.channel.send(`No song found for ${args[0]}`);
         });
-        }
+        
         let song = await client.player.play(message.member.voice.channel, args[0]);
             song.queue.on('end', () => {
             message.channel.send('The queue is empty, leaving voice channel...');
