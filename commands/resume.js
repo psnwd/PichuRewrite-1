88882df44296bf0ec6c61dbc51cc3bc5f5b4d49f
lumap <55940580+lumap@users.noreply.github.com>
2020-02-05 16:41:57 +0100

    Delete resume.js

--- commands/resume.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'resume',
-    description: 'Resumes the current song',
-    category: 'music',
-    async execute(client,message) {
- let song = await client.player.resume(message.guild.id);
-        message.channel.send(`${song.name} resumed!`);
- },
-}
