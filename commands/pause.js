0ab7ce863f62e2b5af5592932788f73ee0d35cee
lumap <55940580+lumap@users.noreply.github.com>
2020-02-05 15:59:38 +0100

    Update pause.js

--- commands/pause.js
+++ commands/pause.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'pause',
     description: 'Pauses the current song',
     category: 'music',
-    execute(client,message) {
+    async execute(client,message) {
         let song = await client.player.pause(message.guild.id);
         message.channel.send(`${song.name} paused!`);
     },
