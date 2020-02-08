5252623e1b541faf78ce43631fbd4cdd3996a919
lumap <55940580+lumap@users.noreply.github.com>
2020-02-08 20:36:45 +0100

    Create cat.js

---
+++ commands/cat.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js')
+
+  module.exports = {
+	name: 'cat',
+  category: 'fun',
+	description: "Shows a cat",
+	execute: async (client,message) => {
+    const res = await require('axios').get('https://api.ksoft.si/images/rand-reddit/cat', {headers: {'Authorization': 'Bearer '+client.tokens.ksoftsi_token})
+      const e = new Discord.MessageEmbed()
+      .setColor('RANDOM')
+      .setDescription(`Image not showing? Click [here](${res.data.image_url}) !`)
+      .setImage(res.data.image_url)
+      .setTimestamp()
+        .setFooter(`Command Ran By: ${message.author.username}`, message.author.avatarURL;
+message.channel.send(e)
+
+	}
+};
