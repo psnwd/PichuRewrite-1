2c308f855ebae6384a2ac2da0526a9f3b2cf7c76
lumap <55940580+lumap@users.noreply.github.com>
2020-02-08 20:41:48 +0100

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -11,7 +11,7 @@ const Discord = require('discord.js')
       .setDescription(`Image not showing? Click [here](${res.data.image_url}) !`)
       .setImage(res.data.image_url)
       .setTimestamp()
-        .setFooter(`Command Ran By: ${message.author.username}`, message.author.avatarURL;
+        .setFooter(`Command Ran By: ${message.author.username}`, message.author.avatarURL;)
 message.channel.send(e)
 
 	}
