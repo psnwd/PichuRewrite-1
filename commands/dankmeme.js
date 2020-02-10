34ee15124724da120d45bba6f4003c597791e734
lumap <55940580+lumap@users.noreply.github.com>
2020-02-10 07:30:01 +0100

    Update and rename pichu.js to dankmeme.js

--- commands/dankmeme.js
+++ commands/dankmeme.js
@@ -1,11 +1,12 @@
 const Discord = require('discord.js')
 
   module.exports = {
-	name: 'pichu',
+	name: 'dankmeme',
+aliases: ['meme'], 
   category: 'fun',
-	description: "Shows a pichu",
+	description: "Shows a meme",
 	execute: async (client,message) => {
-    const res = await require('axios').get('https://api.ksoft.si/images/rand-reddit/pichu', {headers: {'Authorization': 'Bearer '+client.ksoftsi}})
+    const res = await require('axios').get('https://api.ksoft.si/images/rand-reddit/dankmemes', {headers: {'Authorization': 'Bearer '+client.ksoftsi}})
       const e = new Discord.MessageEmbed()
       .setColor('RANDOM')
       .setDescription(`Image not showing? Click [here](${res.data.image_url}) !`)
