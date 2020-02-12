c72d661f75805ff0e7a054aa15ebec95d4dfb6c6
lumap <55940580+lumap@users.noreply.github.com>
2020-02-12 16:52:51 +0100

    Create invite.js

---
+++ commands/invite.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: 'invite',
+  category: 'misc',
+	description: "Add me in your server!",
+	execute(client,message) {
+    message.channel.send('Check your DMs :)')
+		message.author.send(`https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot`)
+	},
+};
