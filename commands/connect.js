4bf5f242085d7f2b95dc9de2df09320b61da4eb7
lumap <55940580+lumap@users.noreply.github.com>
2020-02-16 20:22:38 +0100

    Create connect.js

---
+++ commands/connect.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: 'connect',
+  category: 'music',
+  aliases: ['summon']
+	description: 'Connects to the VC',
+	execute(client,message) {
+		let voiceChannel = message.member.voice.channel
+    if (!voiceChannel) return message.channel.Send('You\'re not connected to a voice channel!')
+    voiceChannel.join()
+    message.channel.send('Here we go! I\'m connected!')
+	},
+};
