c8ab7176dcabc5e20d67d2406bbb82848195b092
lumap <55940580+lumap@users.noreply.github.com>
2020-02-16 20:25:00 +0100

    Create stop.js

---
+++ commands/stop.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: 'stop',
+  category: 'music',
+	description: 'Stop the current song AND the queue',
+	execute(client,message) {
+		const serverQueue = message.client.queue.get(message.guild.id);
+		if (!message.member.voiceChannel) return message.channel.send("You're not in a vocal channel");
+		serverQueue.songs = [];
+		serverQueue.connection.dispatcher.end();
+    message.channel.send('Music successfully stopped') 
+	},
+};
