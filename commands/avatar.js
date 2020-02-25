7136f2c91cbfb1d34627b85d81007f8cc2afa9a5
lumam <lumapbk@gmail.com>
2020-02-25 15:41:57 +0100

    Fixed bug in avatar (again)

--- commands/avatar.js
+++ commands/avatar.js
@@ -5,11 +5,11 @@ module.exports = {
 	description: 'Shows user avatar',
 	execute(client,message,args) {
         const Discord = require('discord.js')
-
+            let avatar = message.author.avatarURL({format: 'png',size: '1024'})
             let avataremb = new Discord.MessageEmbed()
             .setColor('RANDOM')
-            .setDescription(`[Click here to download](${message.author.avatarURL({format: 'jpg',size: '2048'})})`)
-            .setImage(message.author.avatarURL({format: 'jpg',size: '2048'}))
+            .setDescription(`[Click here to download](${avatar})`)
+            .setImage(avatar)
             message.channel.send(avataremb)
         
 
