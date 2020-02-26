cbdf1f69f19b1c70474f3ce76acd320e4fb9a6de
lumam <lumapbk@gmail.com>
2020-02-26 11:46:57 +0100

    server invite command rewrite try#1

--- commands/server-invite.js
+++ commands/server-invite.js
@@ -2,10 +2,10 @@ module.exports = {
     name: 'server-invite',
     category: 'fun',
       description: 'Creates an invite of YOUR server',
-      execute(client,message,args) {
+      async execute(client,message,args) {
           const Discord = require('discord.js')
   
-          async function replyWithInvite(message) {
+         try {
             let invite = await message.channel.createInvite(
             {
               maxAge: 86400, // maximum time for the invite, in milliseconds
@@ -13,12 +13,12 @@ module.exports = {
             },
             `Requested with command by ${message.author.tag}`
           )
-          .catch(console.log);
+          
           
             message.reply(invite ? `Here's your invite: ${invite}` : "There has been an error during the creation of the invite.");
+          } catch (err) {
+            message.channel.send('An error happend during the command : '+err)
           }
-          replyWithInvite(message)
-          
   
       },
   };
