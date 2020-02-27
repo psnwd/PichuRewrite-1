d102d29ed88b88f43aac57a5ced4e78bae9d0c97
lumam <lumapbk@gmail.com>
2020-02-27 12:00:31 +0100

    V1.0.1 : -Random command now requires the user to vote -Changelog command -DJS v11.5.1 used instead of v12

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ const args = message.content.slice(client.prefix.length).split(' ').slice(1)
          if (command.category === 'owner') {
           if (message.author.id !== client.ownerID) return message.reply("You tried to execute a owner-only command, and you can't do that :(")
          }
-            command.execute(client,message,args)
+            command.execute(client,message,args,dbl)
          
         } catch (error) {
             console.log(error)
