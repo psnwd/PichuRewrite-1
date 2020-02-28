c79e05f98d809e08838179559ed6cd933453b1c9
lumam <lumapbk@gmail.com>
2020-02-28 14:58:57 +0100

    music for djs v11

--- commands/play.js
+++ commands/play.js
@@ -70,7 +70,7 @@ module.exports = {
 			return;
 		}
 	
-		const dispatcher = serverQueue.connection.play(ytdl(song.url))
+		const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
 			.on('end', () => {
 				console.log('Finish!');
 				serverQueue.songs.shift();
