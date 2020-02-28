7663ca0e0750eb22446cc51e176e3c74f23191dc
lumam <lumapbk@gmail.com>
2020-02-28 16:00:35 +0100

    dont work, wollback to old command handler

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ client.version = require('./package.json').version
 //
 //
 //Part to customize if you want to selfhost the bot
-client.ownerID = process.env.ownerID //insert ID here
-client.prefix = process.env.prefix //insert prefix here 
-client.bot_token = process.env.bot_token //insert bot token here
-client.dbl_token = process.env.dbl_token
-client.ksoftsi = process.env.ksoftsi_token
+client.ownerID = '635383782576357407'//insert ID here
+client.prefix = 'pi '//insert prefix here 
+client.bot_token = 'Njc0NDk3NjM1MTcxNjk2NjQ0.XlaiYg.xeUN6-KlQWC4G9Wwd_j4WOlhbf8'//insert bot token here
+client.dbl_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3NDQ5NzYzNTE3MTY5NjY0NCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTgyNzEyNjMzfQ.Ovr1uUKctExQ9LKLNDhHFPPPcGgbKiN8hxalRxv6HSk'
+client.ksoftsi = '7bc7dd120e05fc2390ab5494a527a8a0577d0ae0';
 //
 //
 //
@@ -76,13 +76,12 @@ client.on('message', async message =>{
  
  
     if (message.channel.type === "dm" || message.author.bot ||    message.author === client.user) return;
-let prefixlist = [client.prefix, `<@${client.user.id}> `, `<@!${client.user.id}> `]
-for (let i = 0; i !== prefixlist.length; i++) {
-  if (!message.content.toLowerCase().startsWith(prefixlist[i])) {return}
-  else {let args = message.content.slice(prefixlist[i].length).split(' ').slice(1)
-  let commandName = message.content.slice(prefixlist[i].length).split(' ')[0]
-     const command = client.commands.get(commandName)
-            || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName))
+    if (!message.content.toLowerCase().startsWith(client.prefix) || !message.content.startsWith(`<@${client.user.id}> `) || !message.content.startsWith(`<@!${client.user.id}> `)) return;
+   let totalargs = message.content.toLowerCase().startsWith(client.prefix) || message.content.startsWith(`<@${client.user.id}> `) || message.content.startsWith(`<@!${client.user.id}> `)
+        const commandName = totalargs.toLowerCase().split(' ')[0].toLowerCase()
+const args = message.content.slice(client.prefix.length || `<@${client.user.id}> `.length || `<@!${client.user.id}> `.length).split(' ').slice(1)
+        const command = client.commands.get(commandName)
+            || client.commands.find(cmd => cmd.aliases &&                       cmd.aliases.includes(commandName))
         if (!command) return;
         try {
          if (command.category === 'owner') {
@@ -93,9 +92,7 @@ for (let i = 0; i !== prefixlist.length; i++) {
         } catch (error) {
             console.log(error)
         }
-      }
     }
-  }
 );
 
 client.login(client.bot_token)
