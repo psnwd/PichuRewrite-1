c402dbce7683536649709e6705ab144022affed7
lumam <lumapbk@gmail.com>
2020-02-28 14:52:14 +0100

    github broken

--- commands/changelog.js
+++ commands/changelog.js
@@ -4,7 +4,7 @@ module.exports = {
     category: 'misc',
       description: 'Shows a link to my github',
       async execute(client,message,args) {
-          message.channel.send('Hi! To see my changelog, you need to go visit my github repo. https://github.com/lumap/PichuµRewrite')
+          message.channel.send('Hi! To see my changelog, you need to go visit my github repo. https://github.com/lumap/PichuRewrite')
           },
   
       };
