24a57fedc1480e1796fea6db5cc0eecf571d0c0f
lumam <lumapbk@gmail.com>
2020-03-01 22:49:07 +0100

    bug fix

--- commands/stats.js
+++ commands/stats.js
@@ -21,14 +21,15 @@ if (!playingmusic) playingmusic = 0
     const embed1 = new Discord.MessageEmbed()
     .setColor('#5147FF')
     .setAuthor('Bot stats: ')
-    .addField('Uptime : ', `${days} days, ${hours} hours, ${minutes} minutes and ${Math.round(seconds)} seconds`)
-    .addField('Guilds : ', `${client.guilds.cache.size}`)
-    .addField('Bot version : ', `${client.version}`)
-    .addField('Total users :', h) 
-    .addField('Number of servers playing music thx to me :', playingmusic)
-    .addField('Number of commands : ', client.commands.size)
-    .setTimestamp()
-        .setFooter('Made by Lumap#0149')
+    .addFields(
+      {name: 'Uptime :', value: `${days} days, ${hours} hours, ${minutes} minutes and ${Math.round(seconds)} seconds`, inline: true},
+      {name: 'Guilds :', value: client.guilds.cache.size, inline: true},
+      {name: 'Bot version :', value: client.version, inline: true},
+      {name: 'Total users :', value: h, inline: true},
+      {name: 'Number of servers playingmusic thx to me :', value: playingmusic, inline: true},
+      {name: 'number of commands :', value: client.commands.size, inline: true}
+    )
+    .setFooter('Made by Lumap#0149')
     
     message.channel.send(embed1)
   },
