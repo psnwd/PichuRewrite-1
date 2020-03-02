747fe1bd6d6ff8c6cd8f58d7589af43e61e3a69a
lumam <lumapbk@gmail.com>
2020-03-02 17:59:07 +0100

    test

--- commands/help.js
+++ commands/help.js
@@ -8,32 +8,32 @@ module.exports = {
             const embed = new Discord.MessageEmbed()
             .setColor('RANDOM')
             .setTitle('Pichu\'s help!')
-            let misccommands = ''
-            let ownercommands = ''
-            let musiccommands=''
-            let funcommands=''
-            let utilitycommands = ''
+            let misccommands = []
+            let ownercommands = []
+            let musiccommands=[] 
+            let funcommands=[]
+            let utilitycommands = []
             client.commands.forEach(command => {
                             if (!command.aliases) {
-                            if (command.category === 'misc') misccommands+=`\`${command.name}\``
-                            if (command.category === 'owner') ownercommands+=`\`${command.name}\``
-                            if (command.category === 'music') musiccommands+=`\`${command.name}\``
-                            if (command.category === 'fun') funcommands+=`\`${command.name}\``
-                            if (command.category === 'utility') utilitycommands+=`\`${command.name}\``
+                            if (command.category === 'misc') misccommands.push(`\`${command.name}\``)
+                            if (command.category === 'owner') ownercommands.push(`\`${command.name}\``)
+                            if (command.category === 'music') musiccommands.push(`\`${command.name}\``)
+                            if (command.category === 'fun') funcommands.push(`\`${command.name}\``)
+                            if (command.category === 'utility') utilitycommands.push(`\`${command.name}\``)
                             } else {
-                                if (command.category === 'misc') misccommands+=`\`${command.name}(${command.aliases[0]})\``
-                                if (command.category === 'owner') ownercommands+=`\`${command.name}(${command.aliases[0]})\``
-                                if (command.category === 'music') musiccommands+=`\`${command.name}(${command.aliases[0]})\``
-                                if (command.category === 'utility') utilitycommands+=`\`${command.name}(${command.aliases[0]})\``
-                                if (command.category === 'fun') funcommands+=`\`${command.name}(${command.aliases[0]})\``
+                                if (command.category === 'misc') misccommands.push(`\`${command.name}(${command.aliases[0]})\``)
+                                if (command.category === 'owner') ownercommands.push(`\`${command.name}(${command.aliases[0]})\``)
+                                if (command.category === 'music') musiccommands.push(`\`${command.name}(${command.aliases[0]})\``)
+                                if (command.category === 'utility') utilitycommands.push(`\`${command.name}(${command.aliases[0]})\``)
+                                if (command.category === 'fun') funcommands.push(`\`${command.name}(${command.aliases[0]})\``)
                             } 
             })
         embed.addFields(
-            {name: 'Misc commands :', value: misccommands},
-            {name: 'Owner commands :', value: ownercommands},
-            {name: 'Music commands :', value: musiccommands},
-            {name: 'Fun commands :',value: funcommands},
-            {name: 'Utility commands :', value: utilitycommands}
+            {name: 'Misc commands :', value: misccommands.join(', ')},
+            {name: 'Owner commands :', value: ownercommands.join(', ')},
+            {name: 'Music commands :', value: musiccommands.join(', ')},
+            {name: 'Fun commands :',value: funcommands.join(', ')},
+            {name: 'Utility commands :', value: utilitycommands.join(', ')}
             )
 
             message.channel.send(embed)
