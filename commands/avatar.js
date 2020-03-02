bd8ba0534c93ee2d94a391bd64cf630f9d59b294
lumam <lumapbk@gmail.com>
2020-03-02 22:17:02 +0100

    bug fix

--- commands/avatar.js
+++ commands/avatar.js
@@ -4,8 +4,8 @@ module.exports = {
   category: 'utility',
 	description: 'Shows user avatar',
 	async execute(client,message,args) {
-let user = client.users.cache.get(args.join(' ')) || message.mentions.users.first() || message.author 
-
+    let user = message.mentions.users.first() || message.guild.members.cache.find(member => member.user.id === args.join(' '))|| message.guild.members.cache.find(member => member.nickname.toLowerCase() === args.join(' ').toLowerCase() || member.user.username.toLowerCase() === args.join(' ').toLowerCase()) || message.author;
+ 
         const Discord = require('discord.js')
             let avatar = user.avatarURL({format: 'png', dynamic: true, size: 2048})
             let avataremb = new Discord.MessageEmbed()
