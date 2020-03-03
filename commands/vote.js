ef3e6478b3ca06d72627ab83440c697348163ea5
lumap <55940580+lumap@users.noreply.github.com>
2020-03-03 09:48:56 +0100

    Bug fix

--- commands/vote.js
+++ commands/vote.js
@@ -3,7 +3,7 @@ module.exports = {
     category: 'misc',
       description: 'Shows a link to vote for me!',
       async execute(client,message,args) {
-          message.channel.send('Hi! If you want to vote for me, click here :https://top.gg/bot/674497635171696644/vote')
+          message.channel.send('Hi! If you want to vote for me, click here : https://top.gg/bot/674497635171696644/vote')
           },
   
       };
