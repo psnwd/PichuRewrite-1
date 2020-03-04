feb0f343dd25c3ade7aa02f752b9c1a663e664fd
lumam <lumapbk@gmail.com>
2020-03-04 19:28:57 +0100

    test

--- commands/invite.js
+++ commands/invite.js
@@ -3,7 +3,6 @@ module.exports = {
   category: 'misc',
 	description: "Add me in your server!",
 	async execute(client,message) {
-    message.channel.send('Check your DMs :)')
-		message.author.send(`https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot`)
+		message.channel.send(`You want to add me in a server? Well, here is my invite link! https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot`)
 	},
 };
