fff2439ee53b180ac7a2d6244a4b0c2ae332f8c3
lumam <lumapbk@gmail.com>
2020-03-04 13:13:36 +0100

    added volume command

---
+++ commands/volume.js
@@ -0,0 +1,24 @@
+module.exports = {
+    name: 'voluma',
+    category: 'music',
+      description: 'Changes the volume',
+      async execute(client,message,args,dbl,queue) {
+
+        if (!message.member.voice.channel) return message.channel.send("❌ You are not in a voice channel!")
+
+  const serverQueue = queue.get(message.guild.id)
+  if (!serverQueue) return message.channel.send("❌ There is nothing playing right now!")
+
+  if (!args[0]) return message.channel.send("🔉 The volume is " + serverQueue.volume);
+
+  const volume = parseInt(args[0])
+  if (!volume || volume > 100) return message.channel.send("❌ Invalid volume level, pick a number between 1 and 100!")
+
+  serverQueue.volume = volume;
+  serverQueue.connection.dispatcher.setVolumeLogarithmic(volume / 250);
+  return message.channel.send("🔊 The volume is now " + volume + "!")
+          
+  
+      },
+  };
+
