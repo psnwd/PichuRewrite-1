362c08447f2fb2ba5cd143c0a5d70eddce8aad93
lumam <lumapbk@gmail.com>
2020-03-04 13:56:56 +0100

    Added embeds

--- commands/play.js
+++ commands/play.js
@@ -14,15 +14,24 @@ module.exports = {
 		if (!voiceChannel) return message.channel.send("❌ You are not in a voice channel, please join one first!")
 	  
 		const permissions = voiceChannel.permissionsFor(message.guild.me)
-		if (!permissions.has("CONNECT")) return message.channel.send("❌ I don't have permission to connect to the voice channel!")
-		if (!permissions.has("SPEAK")) return message.channel.send("❌ I don't have permission to speak in the voice channel!")
+		if (!permissions.has("CONNECT")) return message.channel.send(new Discord.MessageEmbed()
+		.setColor('RANDOM')
+		.setDescription('I don\'t have the permissions to connect to a voice channel')
+		.setFooter('Made by Lumap#0149'))
+		if (!permissions.has("SPEAK")) return message.channel.send(new Discord.MessageEmbed()
+		.setColor('RANDOM')
+		.setDescription('I don\'t have the permissions to talk here!')
+		.setFooter('Made by Lumap#0149'))
 	  
 		const url = args.join(" ")
 		if (url.includes("list=")) {
 		  const playlist = await ytpl(url.split("list=")[1])
 		  const videos = playlist.items;
 	  
-		  message.channel.send("✅ Playlist **" + playlist.title + "** (" + videos.length + ") has been added to the queue!")
+		  message.channel.send(new Discord.MessageEmbed()
+		  .setColor('RANDOM')
+		  .setDescription(`${playlist.title}(${videos.length} songs) has been added to the queue!`)
+		  .setFooter('Made by Lumap#0149 | Hint : playlists max length is 100'))
 	  
 		  for (const video of videos) await queueSong(video, message, voiceChannel, queue)
 		} else {
@@ -34,32 +43,43 @@ module.exports = {
 			  const results = await ytsr(url)
 			  const videos = results.videos.slice(0, 10)
 			  let index = 0;
-			  await message.channel.send([
+			  await message.channel.send(new Discord.MessageEmbed()
+			  .setColor('RANDOM')
+			  .setDescription([
 				"__**Song selection:**__",
 				videos.map(v => ++index + " - **" + v.title + "**").join("\n"),
 				"**Select your song by sending the number from 1 to " + videos.length + " in chat.**"
 			  ].join("\n\n"))
+			  .setFooter('Made by Lumap#0149 | You have 15 seconds to give a response'))
 	  
 			  let response;
 			  try {
 				response = await message.channel.awaitMessages(msg => 0 < msg.content && msg.content < videos.length + 1 && msg.author.id == message.author.id, {
 				  max: 1,
-				  time: 10000,
+				  time: 15000,
 				  errors: ['time']
 				});
 			  } catch(e) {
 				  console.log(e)
-				return message.channel.send("❌ Video selection timed out.")
+				return message.channel.send(new Discord.MessageEmbed()
+				.setColor('RANDOM')
+				.setDescription('Video selection timed out, cancelling it...')
+				.setFooter('Made by Lumap#0149'))
 			  }
 			  const videoIndex = parseInt(response.first().content)
 			  video = await ytdl.getBasicInfo(videos[videoIndex - 1].videoId)
 			} catch(e) {
 			  console.log(e)
-			  return message.channel.send("❌ No search results found.")
+			  return message.channel.send(new Discord.MessageEmbed()
+			  .setColor('RANDOM')
+			  .setDescription('I wasn\'t able to find a result :(')
+			  .setFooter('Made by Lumap#0149'))
 			}
 		  }
 		  
-		  await message.channel.send("✅ Song **" + video.title + "** has been added to the queue!")
+		  await message.channel.send(new Discord.MessageEmbed()
+		  .setColor('RANDOM')
+		  .setDescription(`**[${video.title}](${video.url})** has been added to the queue!`))
 		  return await queueSong(video, message, voiceChannel, queue)
 		}
 	  
@@ -90,7 +110,7 @@ module.exports = {
 			playSong(message.guild, queue, queueConstruct.songs[0])
 		  } catch(e) {
 			console.log(e)
-			message.channel.send("❌ An unknown error occoured upon trying to join the voice channel!")
+			message.channel.send('An error happened while trying to join the voice channel, please try again')
 			return queue.delete(message.guild.id)
 		  }
 		} else serverQueue.songs.push(song);
@@ -115,7 +135,9 @@ module.exports = {
 		  .on("error", console.error)
 		  .setVolumeLogarithmic(serverQueue.volume / 250)
 		
-		serverQueue.textChannel.send("🎶 Now playing **" + song.title + "**")
+		serverQueue.textChannel.send(new Discord.MessageEmbed()
+		.setColor('RANDOM')
+		.setDescripton(`Now playing **[${song.title}](${song.url})**`))
 	  }
     },
 }
--- commands/volume.js
+++ commands/volume.js
@@ -1,22 +1,38 @@
+const Discord = require('discord.js')
 module.exports = {
     name: 'volume',
     category: 'music',
       description: 'Changes the volume',
       async execute(client,message,args,dbl,queue) {
 
-        if (!message.member.voice.channel) return message.channel.send("❌ You are not in a voice channel!")
+        if (!message.member.voice.channel) return message.channel.send(new Discord.MessageEmbed()
+        .setColor('RANDOM')
+        .setDescription('You are not in a voice channel!')
+        .setFooter('Made by Lumap#0149'))
 
   const serverQueue = queue.get(message.guild.id)
-  if (!serverQueue) return message.channel.send("❌ There is nothing playing right now!")
+  if (!serverQueue) return message.channel.send(new Discord.MessageEmbed()
+  .setColor('RANDOM')
+  .setDescription('Nothing is playing right now :(')
+  .setFooter('Made by Lumap#0149'))
 
-  if (!args[0]) return message.channel.send("🔉 The volume is " + serverQueue.volume);
+  if (!args[0]) return message.channel.send(new Discord.MessageEmbed()
+  .setColor('RANDOM')
+  .setDescription(`The current volume is **${serverQueue.volume}**`)
+  .setFooter('Made by Lumap#0149'));
 
   const volume = parseInt(args[0])
-  if (!volume || volume > 100) return message.channel.send("❌ Invalid volume level, pick a number between 1 and 100!")
+  if (!volume || volume > 100) return message.channel.send(new Discord.MessageEmbed()
+  .setColor('RANDOM')
+  .setDescription('Invalid volume! Please provide a number between 1 and 100')
+  .setFooter('Made by Lumap#0149'))
 
   serverQueue.volume = volume;
   serverQueue.connection.dispatcher.setVolumeLogarithmic(volume / 250);
-  return message.channel.send("🔊 The volume is now " + volume + "!")
+  return message.channel.send(new Discord.MessageEmbed()
+  .setColor('RANDOM')
+  .setDescrition(`The volume is now **${volume}**!`)
+  .setFooter('Made by Lumap#0149'))
           
   
       },
