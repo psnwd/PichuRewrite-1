9d90f14f02444d076d26549914d53adfc497df85
lumam <lumapbk@gmail.com>
2020-03-04 14:32:30 +0100

    requested

---
+++ commands/evolve.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: 'evolve',
+  category: 'misc',
+	description: 'Pichu is now a pikachu!',
+	async execute(client,message) {
+        const Discord = require('discord.js')
+        let embed = new Discord.MessageEmbed()
+        .setColor('RANDOM')
+        .setDescription('Pichu is evolving... See how it evolves [here !](https://www.youtube.com/watch?v=dQw4w9WgXcQ)')
+        .setFooter('Made by Lumap#0149')
+        
+	},
+};
