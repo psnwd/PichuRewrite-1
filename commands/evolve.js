0026a7f337174837b110eb6a80e4f6a8f963cd3a
lumam <lumapbk@gmail.com>
2020-03-04 14:33:41 +0100

    requested

--- commands/evolve.js
+++ commands/evolve.js
@@ -8,6 +8,6 @@ module.exports = {
         .setColor('RANDOM')
         .setDescription('Pichu is evolving... See how it evolves [here !](https://www.youtube.com/watch?v=dQw4w9WgXcQ)')
         .setFooter('Made by Lumap#0149')
-        
+        message.channel.send(embed)
 	},
 };
