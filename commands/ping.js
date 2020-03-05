3781f46833120dbc92a68de4ccb02c193286d868
lumam <lumapbk@gmail.com>
2020-03-05 16:41:35 +0100

    test

--- commands/ping.js
+++ commands/ping.js
@@ -4,6 +4,6 @@ module.exports = {
     description: 'Ping? Pong! Shows your lag',
     category: 'misc',
     async execute(client,message) {
-        message.channel.send(`Pong! My ping is **${client.ws.ping}** ms!`)
+        message.channel.send(`Pong! API ping is **${client.ws.ping}** ms!`)
     },
 }
