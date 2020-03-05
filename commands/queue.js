dc217cf235e232784e8425f188ca4493b48ff4df
lumam <lumapbk@gmail.com>
2020-03-05 15:36:01 +0100

    open queue

--- commands/queue.js
+++ commands/queue.js
@@ -9,7 +9,7 @@ module.exports = {
       async execute(client,message,args,dbl,queue) {
 
         const serverQueue = queue.get(message.guild.id);
-		if (!message.member.voice.channel) return message.channel.send('Please add me in a voice channel');
+
         if (!serverQueue) return message.channel.send('The queue is empty!');
         const output = []
         for (let i = 1; i < Math.min(serverQueue.songs.length, 11); i++) {
