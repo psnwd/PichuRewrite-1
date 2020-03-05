8b27c8856969d009d4818e234bf9d23827738567
lumam <lumapbk@gmail.com>
2020-03-05 16:01:13 +0100

    should delete previous 'now playing' messages, even if queue is stopped'

--- commands/stop.js
+++ commands/stop.js
@@ -9,6 +9,7 @@ module.exports = {
 		if (!message.member.voice.channel) return message.channel.send("You're not in a vocal channel");
 		if (serverQueue.songs[0].author !== message.author) return message.channel.send(new Discord.MessageEmbed() .setColor('RANDOM') .setDescription(`Only ${serverQueue.songs[0].author} can stops the queue, beacause he requested the current song`))
 		serverQueue.songs = [];
+		serverQueue.message.delete();
 		serverQueue.connection.dispatcher.end();
     message.channel.send('Music successfully stopped') 
 	},
