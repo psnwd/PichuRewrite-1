94127507eda5b84b09fc61625482d9bcacea5bc2
lumam <lumapbk@gmail.com>
2020-03-06 17:01:50 +0100

    added owoify

---
+++ commands/owoify.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: 'owoify',
+    description: 'Transforms a text into a owo text',
+    usage: 'pichu owoify <text>',
+    aliases: ['owotext'],
+    category: 'fun',
+    async execute(client,message,args) {
+        const nekoclient = require('nekos.life');
+const neko = new nekoclient();
+if (!args.join(' ')) return message.channel.send('Please provide a text.')
+async function work() {
+  let owo = await neko.sfw.OwOify({text: args.join(' ')});
+  message.delete().then(message.channel.send(owo));
+}
+
+work();
+    }
+}
