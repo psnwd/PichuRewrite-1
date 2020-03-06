55a329b0aab47a9e594828f731d0af823d1318c3
lumam <lumapbk@gmail.com>
2020-03-06 17:09:41 +0100

    bug fix

--- commands/owoify.js
+++ commands/owoify.js
@@ -6,13 +6,12 @@ module.exports = {
     category: 'fun',
     async execute(client,message,args) {
         const nekoclient = require('nekos.life');
-const neko = new nekoclient();
-if (!args.join(' ')) return message.channel.send('Please provide a text.')
-async function work() {
-  let owo = await neko.sfw.OwOify({text: args.join(' ')});
-  message.delete().then(message.channel.send(owo.owo));
-}
-
-work();
+        const neko = new nekoclient();
+        if (!args.join(' ')) return message.channel.send('Please provide a text.')  
+        async function work() {
+            let owo = await neko.sfw.OwOify({text: args.join(' ')});
+            message.delete().then(message.channel.send(owo.owo));
+        }
+        work();
     },
-}
+};
