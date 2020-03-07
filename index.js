97cc33fc6f494086410a61e14fa6aa9d68a3be6b
lumam <lumapbk@gmail.com>
2020-03-07 23:12:38 +0100

    dbl webhooks works!

--- index.js
+++ index.js
@@ -44,8 +44,8 @@ dbl.webhook.on('error', e => {
 })
 
 dbl.webhook.on('vote', vote => {
-  user = client.users.get(vote.user)
-  client.channels.get('685971547315240969').send(new Discord.MessageEmbed() .setColor('RANDOM') .setThumbnail(user.avatarURL({format: 'png', dynamic: true, size: 2048})) .setDescription(`Thanks you for voting <@${user.id}> (**${user.id}**)! As a reward, you get... Respect lol`))
+  user = client.users.cache.get(vote.user)
+  client.channels.cache.get('685971547315240969').send(new Discord.MessageEmbed() .setColor('RANDOM') .setThumbnail(user.avatarURL({format: 'png', dynamic: true, size: 2048})) .setDescription(`Thanks you for voting <@${user.id}> (**${user.id}**)! As a reward, you get... Respect lol`))
 });
 
 
