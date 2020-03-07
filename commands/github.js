f6317f0e86ebdefacfb5cb91ceec52e9761b5bc9
lumap <55940580+lumap@users.noreply.github.com>
2020-03-07 18:31:45 +0100

    Github repo reopened

--- commands/github.js
+++ commands/github.js
@@ -4,7 +4,7 @@ module.exports = {
     usage: 'pichu github',
       description: 'Shows a link to my github',
       async execute(client,message,args) {
-          message.channel.send('Hi! My owner make my github repo private, so it\'s impossible for anyone to acces it now. Maybe one day? ')
+          message.channel.send('Hi! Here is my girhub repo : https://github.com/lumap/PichuRewrite')
           },
   
       };
