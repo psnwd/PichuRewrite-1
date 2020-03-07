2f65c21022935a8aa594d36878193608c0ea0e0e
lumam <lumapbk@gmail.com>
2020-03-07 20:41:54 +0100

    3.0.2 : vote lock some commands

--- commands/volume.js
+++ commands/volume.js
@@ -32,7 +32,11 @@ module.exports = {
   .setDescription('Invalid volume! Please provide a number between 1 and 150')
   .setFooter('Made by Lumap#0149'))
 
-  serverQueue.volume = volume;
+
+  dbl.hasVoted(message.author.id).then(voted => {
+    if (!voted && volume>100) return message.channel.send(new Discord.MessageEmbed() .setColor('RANDOM') .setDescription('Hi. If you want to listen to me with 100+ volume, you have to [vote](https://top.gg/bot/674497635171696644/vote) for me! (having 100+ volume lags the bot)') .setFooter('Made by Lumap#0149'))
+});
+serverQueue.volume = volume;
   serverQueue.connection.dispatcher.setVolumeLogarithmic(volume / 250);
   return message.channel.send(new Discord.MessageEmbed()
   .setColor('RANDOM')
