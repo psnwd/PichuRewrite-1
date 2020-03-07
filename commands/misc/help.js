23addae09fb7d8c1fc9d3d1a0f91da9dcf723ddc
lumam <lumapbk@gmail.com>
2020-03-08 00:33:32 +0100

    new help command

--- commands/misc/help.js
+++ commands/misc/help.js
@@ -33,7 +33,9 @@ function getAll(client, message) {
 //random color already exists
 function getCMD(client, message, input) {
     const embed = new Discord.MessageEmbed()
-    if (!client.commands.get(input.toLowerCase()) || !client.aliases.get(input.toLowerCase())) return message.channel.send(`No information found for command **${input.toLowerCase()}**`)
+    if (!client.commands.get(input.toLowerCase())){ 
+        if (!client.aliases.get(input.toLowerCase())) return message.channel.send(`No information found for command **${input.toLowerCase()}**`)
+    }
     const cmd = client.commands.get(input.toLowerCase()) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName))
 
     if (cmd.name) info = `**Command name**: ${cmd.name}`;
