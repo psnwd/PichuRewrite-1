fc6b1ab94a6c428330bcf5da88c33b8d6025a07c
lumam <lumapbk@gmail.com>
2020-03-08 15:10:29 +0100

    snipe

--- commands/moderation/snipe.js
+++ commands/moderation/snipe.js
@@ -6,10 +6,11 @@ module.exports = {
     description: 'Get the last deleted message',
     category: 'moderation',
     async execute(client,message) {
-    if (!client.snipes.get(message.channel.id)) return message.channel.send("There is nothing to snipe")
+        const channel = message.mentions.channels.first() || message.channel
+    if (!client.snipes.get(channel.id)) return message.channel.send("There is nothing to snipe")
     const embed = new Discord.MessageEmbed()
-    .setTitle(client.snipes.get(message.channel.id).user.tag)
-    .setDescription(client.snipes.get(message.channel.id).content)
+    .setTitle(client.snipes.get(channel.id).user.tag)
+    .setDescription(client.snipes.get(channel.id).content)
     .setColor("RANDOM")
     .setFooter('Made by Link#4444')
     message.channel.send(embed)
