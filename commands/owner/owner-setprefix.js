89d53d9ba606cac3765196c606c76e37b2e28d5c
lumap <55940580+lumap@users.noreply.github.com>
2020-03-08 18:59:04 +0100

    Update and rename ownersetprefix.js to owner-setprefix.js

--- commands/owner/owner-setprefix.js
+++ commands/owner/owner-setprefix.js
@@ -2,9 +2,8 @@ module.exports = {
     name: 'owner-setprefix',
     usage: 'pichu owner-setprefix <new prefix>',
     description: 'Changes pichu\'s prefix on this server',
-    category: 'utility',
+    category: 'owner',
     execute(client,message,args) {
-        if (!message.author.id === client.ownerID) return message.channel.send('Only servers admins can change my prefix');
         if (!args.join(' ')) return message.channel.send('Please provide a new prefix between 1 and 15 chars!');
         args.join(' ');
         if (args.length < 1 || args.length > 15) return message.channel.send('Please provide a new prefix **between 1 and 15 chars**!');
