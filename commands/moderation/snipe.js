7754f1c0b00e1ad87d88f18a1961ca9a923ef804
lumam <lumapbk@gmail.com>
2020-03-08 11:00:33 +0100

    bug ix

---
+++ commands/moderation/snipe.js
@@ -0,0 +1,10 @@
+const Quick = require("quick.db-plus")
+module.exports = {
+    name: 'snipe',
+    usage: 'pichu snipe',
+    description: 'Get the last deleted message',
+    category: 'moderation',
+    async execute(client,message) {
+
+    }, 
+} 
